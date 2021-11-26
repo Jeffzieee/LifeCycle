11d05df1b6f3de8095224e6841b536dbdef408b0
Jephin John <jephinjohn85@gmail.com>
2021-11-26 15:01:11 +0530

    -->DessertTimer Class Used. -->Timers has been set onStart() and onStop()

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
-
+    private lateinit var dessertTimer : DessertTimer
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
 
@@ -74,7 +74,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
-
+        dessertTimer = DessertTimer()
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
@@ -153,11 +153,16 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStart(){
         super.onStart()
         Timber.i("onStart Called")
-        dessertsSold = 0
+        dessertTimer.startTimer()
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Timber.i("onStop Called")
+        dessertTimer.stopTimer()
     }
 
     override fun onResume(){
         super.onResume()
-        dessertsSold=0
     }
 }
